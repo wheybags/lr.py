2f95aebe0ae12980479468ec10afe9a9e7125109
Tom Mason <wheybags@wheybags.com>
2014-01-03 22:46:26 +0000

    getParser now returns an executable function that will fully parse the input string

--- lr.py
+++ lr.py
@@ -2,20 +2,6 @@ import copy
 
 from token import *
 
-def getFollows(sym, grammar):
-    follows = []
-
-    for p in grammar:
-        for e in grammar[p]:
-            for i in range(len(e)):
-                if e[i] == sym:
-                    if i == len(e)-1:
-                        follows += getFollows(nonTerminal(p), grammar)
-                    else:
-                        follows.append(e[i+1])
-    
-    return follows
-
 class lrExpression(object):
     def __init__(self, expr, la, lrpos):
         self.expr = expr
@@ -61,6 +47,21 @@ class lrState(object):
         return s
 
 
+def getFollows(sym, grammar):
+    follows = []
+
+    for p in grammar:
+        for e in grammar[p]:
+            for i in range(len(e)):
+                if e[i] == sym:
+                    if i == len(e)-1:
+                        follows += getFollows(nonTerminal(p), grammar)
+                    else:
+                        follows.append(e[i+1])
+    
+    return follows
+
+
 def fillState(stateNum, lrpos, states, grammar, follows):
     newStates = set()
     
@@ -89,17 +90,89 @@ def fillState(stateNum, lrpos, states, grammar, follows):
     for s in newStates:
         fillState(s, lrpos, states, grammar, follows)
 
+def genFunc(stateNum, states):
+    state = states[stateNum]
+    
+    func =  "    def func" + str(stateNum) + "(states, stack, input):\n"
+    func += "        state = states[" + str(stateNum) + "]\n"
+    func += "        tok = nextTok(input)\n"
+    func += "        if tok in state:\n"
+    func += "            advance(input)\n"
+    func += "            stack.append(tok)\n"
+    func += "            stack.append(state[tok])\n"
+    func += "            return state[tok]\n"
+
+    for prodnum in range(len(state.productions)):
+        prod = state.productions[prodnum]
+        if prod[1].isReduce():
+            func += "        if lookahead(input) == "+ str(prod[1].la) + " and"
+            for i in range(len(prod[1].expr)):
+                func += " " + str(prod[1].expr[i]) + " == stack[" + str(2*i) + " + abs(len(stack)-" + str(len(prod[1].expr)*2) + ")] and"
+            
+            func = func[:-4]
+            func += ":\n"
+            func += "            del stack[-" + str(2*len(prod[1].expr)) + ":]\n"
+            func += "            input.insert(0, " + str(prod[0]) + ")\n"
+            func += "            # ----- action here\n"
+            func += "            return stack[-1]\n"
+            
+
+    return func
+
 def getParser(grammar):
     
     follows = {}
     for p in grammar:
         follows[p] = getFollows(nonTerminal(p), grammar)
 
-    states = [ lrState([["START'", lrExpression(grammar["START'"][0], [], 0)]]) ]
+    states = [ lrState([[nonTerminal("START'"), lrExpression(grammar["START'"][0], [], 0)]]) ]
 
     fillState(0, 0, states, grammar, follows)
+    
+    code =  "from token import terminal, nonTerminal\n" 
+   
+    code += "def parse(input):\n"
+    
+    code += "    def nextTok(input):\n"
+    code += "        if len(input) == 0:\n"
+    code += "            return None\n"
+    code += "        return input[0]\n"
+    code += "    def advance(input):\n"
+    code += "        retval = input[0]\n"
+    code += "        del input[0]\n"
+    code += "        return retval\n"
+    code += "    def lookahead(input):\n"
+    code += "        return input[0:1]\n"
 
-    return states
+    for i in range(len(states)):
+        code += genFunc(i, states)
+
+    code += "    stack = [0]\n"
+    code += "    states = {"
+    for i in range(len(states)):
+        code += str(i) + ": " + str(states[i].transitions) + ", "
+
+    code = code[:-2]
+    code += "}\n"
+
+    code += "    funcs = {"
+    for i in range(len(states)):
+        code += str(i) + ": func" + str(i) + ", "
+    code = code[:-2]
+    code += "}\n"
+
+    code += "    current = 0\n"
+    code += "    while not (stack == [0] and input == [nonTerminal(\"START'\")]):\n"
+    code += "        print current\n"
+    code += "        print stack\n"
+    code += "        print input\n"
+    code += "        print\n"
+    code += "        current = funcs[current](states, stack, input)\n"
+
+    exec code
+
+    return parse
+
 
 def main():
     grammar = {
@@ -114,12 +187,9 @@ def main():
         "B": [[terminal("d")]]
     }
 
-    parser = getParser(grammar)
+    parse = getParser(grammar)
     
-    for i in range(len(parser)):
-        print i
-        print parser[i].transitions
-        print parser[i].strProductions()
+    parse([terminal("d"), terminal("c"), terminal()])
 
 if __name__ == "__main__":
     main()
