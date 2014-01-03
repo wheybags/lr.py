d523e2873a11c29b98ee900a33a81d3782f1d31e
Tom Mason <wheybags@wheybags.com>
2014-01-03 15:15:11 +0000

    move token classes to seperate file

--- lr.py
+++ lr.py
@@ -1,5 +1,7 @@
 import copy
 
+from token import *
+
 def getFollows(sym, grammar):
     follows = []
 
@@ -98,36 +100,6 @@ def getParser(grammar):
     fillState(0, 0, states, grammar, follows)
 
     return states
-
-
-class token(object):
-    def __init__(self, name):
-        self.name = name
-    def __eq__(self, other):
-        if isinstance(other, type(self)):
-            return self.name == other.name
-    def __str__(self):
-        return type(self).__name__ + "(" + self.name + ")"
-    
-    def __repr__(self):
-        return self.__str__()
-
-    def __hash__(self):
-        return self.__repr__().__hash__()
-
-class terminal(token):
-    
-    def __init__(self, *args, **kwargs):
-        
-        if len(args) == 0:
-            self.end = True
-            args = [""]
-
-        super(terminal, self).__init__(*args, **kwargs)
-
-class nonTerminal(token):
-    pass
-
 
 def main():
     grammar = {
---
+++ token.py
@@ -0,0 +1,27 @@
+class token(object):
+    def __init__(self, name):
+        self.name = name
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return self.name == other.name
+    def __str__(self):
+        return type(self).__name__ + "(" + self.name + ")"
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __hash__(self):
+        return self.__repr__().__hash__()
+
+class terminal(token):
+    
+    def __init__(self, *args, **kwargs):
+        
+        if len(args) == 0:
+            self.end = True
+            args = [""]
+
+        super(terminal, self).__init__(*args, **kwargs)
+
+class nonTerminal(token):
+    pass
